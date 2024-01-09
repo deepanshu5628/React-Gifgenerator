ede505af7a0753440927a0cebe14acc880d4912c
deepanshu5628 <d2810201@gmail.com>
2024-01-09 13:12:55 +0530

    custom div complested

--- src/Component/Random.jsx
+++ src/Component/Random.jsx
@@ -4,18 +4,14 @@ import Customloader from "../Component/Customloader";
 function Random(){
     let {fetchdata,spinner,giflink}=useGif();
 
-    function randombtn(){
-        console.log("button is cliedced");
-        fetchdata();
-        console.log(giflink);
-    }
+    
     return (
         <div className="Random">
             <h2>Random Gif's</h2>
             {spinner? ( <Customloader/>):(<img src={giflink} alt="image" />)}
             
             <br />
-            <button onClick={randombtn}>Generate Random</button>
+            <button onClick={()=>fetchdata()}>Generate Random</button>
         </div>
     )
 }
--- src/useGif.jsx
+++ src/useGif.jsx
@@ -1,14 +1,18 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-function useGif(){
+function useGif(tag){
     let [giflink,setgiflink]=useState("");
     let [spinner,setspinner]=useState(false);
+    let url=`https://api.giphy.com/v1/gifs/random?api_key=VoRooJLpgTITjx1M93eAg8SilRwtWo6w&tag=`;
+    let tagurl=`https://api.giphy.com/v1/gifs/random?api_key=VoRooJLpgTITjx1M93eAg8SilRwtWo6w&tag=${tag}`;
+    
 
-    let fetchdata= async()=>{
+    let fetchdata= async(tag)=>{
+        // console.log(tag? console.log("tag is present"):console.log("tag is not present"));
         setspinner(true);
-        let url="https://api.giphy.com/v1/gifs/random?api_key=VoRooJLpgTITjx1M93eAg8SilRwtWo6w&tag=&rating=g";
-        let output=await axios.get(url);
-        let link=output.data.data.images.fixed_height_still.url;
+        let output=await axios.get(tag? tagurl:url);
+        let link=output.data.data.images.fixed_height_small_still.url;
+        console.log(output.data.data.images);
         setgiflink(link);
         setspinner(false);
     }
